845d827caf14110ae12501170448c05fd35ec8e1
GuilhemGabrielli <guilhem.gabrielli@campus.academy>
2022-01-12 11:46:23 +0100

    Udapte database link

--- js/partenaires.js
+++ js/partenaires.js
@@ -1,4 +1,4 @@
-var requestPartenairesURL = 'https://raw.githubusercontent.com/GuilhemGabrielli/-CampusWorkshop2022-Team04/main/json/partenaires.json';
+var requestPartenairesURL = 'https://raw.githubusercontent.com/CampusWorkshop2022-team04/Digital-Consultant/main/json/partenaires.json?token=GHSAT0AAAAAABQP6BDBLJQ4JY7DWNDEYC6CYO6WFFA';
 
 var requestPartenaires = new XMLHttpRequest();
 var partenaires = []
