fe19cdc0d42a29512bf281100eabd1db0843201b
GuilhemGabrielli <guilhem.gabrielli@campus.academy>
2022-01-12 12:01:41 +0100

    Create contact.js

---
+++ js/contact.js
@@ -0,0 +1,14 @@
+var requestContactURL = 'https://raw.githubusercontent.com/GuilhemGabrielli/-CampusWorkshop2022-Team04/main/json/contact.json';
+
+var requestContact = new XMLHttpRequest();
+var contact = []
+
+requestContact.open('GET', requestContactURL);
+
+requestContact.responseType = 'json';
+requestContact.send();
+
+requestContact.onload = function() {
+    contact = requestContact.response;
+    console.log(contact)
+}
