99ef5e32910b3e374dce995935d1fa5dc5539c69
GuilhemGabrielli <guilhem.gabrielli@campus.academy>
2022-01-13 18:06:37 +0100

    correctifs et articles auto

--- js/articles.js
+++ js/articles.js
@@ -11,6 +11,7 @@ requestArticles.send();
 requestArticles.onload = function() {
     articles = requestArticles.response;
     console.log(articles)
+    presentArticle()
 }
 
 
@@ -20,12 +21,28 @@ function presentArticle() {
 
     if (articles.length == 0) {
         hArticle.innerText = "Désolé, aucun article n'est actuellement disponible"
-        divNosArticles.append(hAucunArticle)
     } else {
         hArticle.innerText = "Nos articles"
+        addArticles(divNosArticles)
     }
+    divNosArticles.prepend(hArticle)
 }
 
-function addArticles() {
+function addArticles(divArticle) {
+    console.log(articles.length)
+    for (let i=0; i<articles.length; i++) {
+        const article = document.createElement('div')
+
+        const auteur = document.createElement('h5')
+        auteur.innerText = articles[i]["auteur"] + " le " + articles[i]["date"]
+
+        const commentaire = document.createElement('p')
+        commentaire.innerText = articles[i]["commentaire"]
+
+        article.append(auteur)
+        article.append(commentaire)
+
+        divArticle.append(article)
+    }
 
 }
