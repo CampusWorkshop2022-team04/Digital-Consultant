331b59a31dc06c6ec39398c7e4926058e8dcfcae
GuilhemGabrielli <guilhem.gabrielli@campus.academy>
2022-01-16 17:54:43 +0100

    update of blog.html

--- js/articles.js
+++ js/articles.js
@@ -16,27 +16,31 @@ requestArticles.onload = function() {
 
 
 function presentArticle() {
-    const divNosArticles = document.getElementById('nosArticles')
+    const main = document.getElementById('main')
     const hArticle = document.createElement('h1')
+    hArticle.id = "titleArticles"
 
     if (articles.length == 0) {
         hArticle.innerText = "Désolé, aucun article n'est actuellement disponible"
     } else {
         hArticle.innerText = "Nos articles"
-        addArticles(divNosArticles)
+        addArticles(document.getElementById('nosArticles'))
     }
-    divNosArticles.prepend(hArticle)
+    main.prepend(hArticle)
 }
 
 function addArticles(divArticle) {
     console.log(articles.length)
     for (let i=0; i<articles.length; i++) {
         const article = document.createElement('div')
+        article.className = "divArticleBlog"
 
         const auteur = document.createElement('h5')
+        auteur.className = "auteurArticleBlog"
         auteur.innerText = articles[i]["auteur"] + " le " + articles[i]["date"]
 
         const commentaire = document.createElement('p')
+        commentaire.className = "commentaireArticleBlog"
         commentaire.innerText = articles[i]["commentaire"]
 
         article.append(auteur)
