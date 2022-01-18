0001275fa980ce4048288fa37efc4344e12b06ad
GuilhemGabrielli <guilhem.gabrielli@campus.academy>
2022-01-18 17:21:25 +0100

    update

--- js/services.js
+++ js/services.js
@@ -11,27 +11,35 @@ requestServices.send();
 requestServices.onload = function() {
     services = requestServices.response;
     console.log(services)
-    presentService()
+    // presentService()
 }
 
-function presentService() {
-    const nomService1 = document.getElementById('nom-service-1')
-    nomService1.innerText = services[0]["nomService"]
+// function presentService() {
+//     const nomService1 = document.getElementById('nom-service-1')
+//     nomService1.innerText = services[0]["nomService"]
 
-    const descriptionService1 = document.getElementById('description-service-1')
-    descriptionService1.innerText = services[0]['descriptionService']
-
-
-    const nomService2 = document.getElementById('nom-service-2')
-    nomService2.innerText = services[1]["nomService"]
-
-    const descriptionService2 = document.getElementById('description-service-2')
-    descriptionService2.innerText = services[1]['descriptionService']
+//     const descriptionService1 = document.getElementById('description-service-1')
+//     descriptionService1.innerText = services[0]['descriptionService']
 
 
-    const nomService3 = document.getElementById('nom-service-3')
-    nomService3.innerText = services[2]["nomService"]
+//     const nomService2 = document.getElementById('nom-service-2')
+//     nomService2.innerText = services[1]["nomService"]
 
-    const descriptionService3 = document.getElementById('description-service-3')
-    descriptionService3.innerText = services[2]['descriptionService']
-}
+//     const descriptionService2 = document.getElementById('description-service-2')
+//     descriptionService2.innerText = services[1]['descriptionService']
+
+
+//     const nomService3 = document.getElementById('nom-service-3')
+//     nomService3.innerText = services[2]["nomService"]
+
+//     const descriptionService3 = document.getElementById('description-service-3')
+//     descriptionService3.innerText = services[2]['descriptionService']
+// }
+
+
+document.getElementById('service1').addEventListener(
+    'click',
+    function () {
+        console.log('test')
+    }
+)
