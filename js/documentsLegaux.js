d68229cd33a3c53746512bbfae596ee226238510
GuilhemGabrielli <guilhem.gabrielli@campus.academy>
2022-01-19 14:19:47 +0100

    update documentsLegaux

--- js/documentsLegaux.js
+++ js/documentsLegaux.js
@@ -10,79 +10,19 @@ requestDocumentsLegaux.send();
 
 requestDocumentsLegaux.onload = function() {
     documentsLegaux = requestDocumentsLegaux.response;
+    modificationPrivatePolicy(documentsLegaux)
 }
 
-// function presentDocumentsLegaux() {
-//     const divBoutonDocumentLegaux = document.getElementById('divBoutonDocumentsLegaux')
-//     for (let i = 0; i < documentsLegaux.length; i++) {
-//         const key = Object.keys(documentsLegaux[i])[0]
-//         const boutonDoument = document.createElement('button')
-//         boutonDoument.id = key
-//         switch (key) {
-//             case 'mentionsLegales' :
-//                 boutonDoument.innerText = "Mentions Légales"
-//                 break;
-//             case 'CGU' :
-//                 boutonDoument.innerText = "Conditions Générales d'Utilisation";
-//                 break;
-//             default :
-//                 boutonDoument.innerText = key;
-//                 break;
-//         }
+function modificationPrivatePolicy(documents) {
 
-//         divBoutonDocumentLegaux.append(boutonDoument)
-//     }
-// }
+    const keyJSON = Object.keys(documents)
+    const keyHTML = documents["classHTML"]
+    for (let i=0; i<keyHTML.length; i++) {
+        arr = document.getElementsByClassName(keyHTML[i])
 
-
-function showMentionsLegals() {
-    const pDocumentLegaux = document.getElementById('pDocumentsLegaux')
-    pDocumentLegaux.innerText = documentsLegaux['mentionsLegales']
-}
-
-function showCGU() {
-    const pDocumentLegaux = document.getElementById('pDocumentsLegaux')
-    pDocumentLegaux.innerText = documentsLegaux['CGU']
-}
-
-function showRGPD() {
-    const pDocumentLegaux = document.getElementById('pDocumentsLegaux')
-    pDocumentLegaux.innerText = documentsLegaux['RGPD']
-}
-
-function showTitleDocument(titleDocument) {
-    const divDocumentsLegaux = document.getElementById('divDocumentsLegaux')
-    // const hTitleDocument = document.createElement('h4')
-    var hTitleDocument = document.getElementById('titleDocument')
-    if (hTitleDocument == null) {
-        hTitleDocument = document.createElement('h4')
-        hTitleDocument.id = 'titleDocument'
+        for (let j=0; j<arr.length; j++) {
+            arr[j].innerText = documents[keyJSON[i+1]]
+        }
     }
-    hTitleDocument.innerText = titleDocument
-    divDocumentsLegaux.prepend(hTitleDocument)
+
 }
-
-
-document.getElementById('btnMentionsLegales').addEventListener(
-    'click',
-    function() {
-        showTitleDocument('Mentions Légales')
-        showMentionsLegals()
-    }
-)
-
-document.getElementById('btnCGU').addEventListener(
-    'click',
-    function() {
-        showTitleDocument('CGU')
-        showCGU()
-    }
-)
-
-document.getElementById('btnRGPD').addEventListener(
-    'click',
-    function() {
-        showTitleDocument('RGPD')
-        showRGPD()
-    }
-)
