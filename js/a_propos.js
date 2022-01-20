129f7295b7a76b975d0753161c04600e1bf471a6
GuilhemGabrielli <guilhem.gabrielli@campus.academy>
2022-01-20 15:24:31 +0100

    Update a_propos.js

--- js/a_propos.js
+++ js/a_propos.js
@@ -1,10 +1,14 @@
+// Variables globales
+var propos = []
+var langueActif = "fr"
+
+// Désactivation de lien possédant l'id "page-actuel", cela empêche l'utilisateur d'ouvrir la page sur laquelle il se trouve
 document.getElementById('page-actuel').removeAttribute('href')
 
+// Partie exécuté au lancement de la page permettant de récupérer les données JSON stockées sur GitHub et de les convertir en JS
 var requestProposURL = 'https://raw.githubusercontent.com/CampusWorkshop2022-team04/Digital-Consultant/main/json/a_propos.json';
 
 var requestPropos = new XMLHttpRequest();
-var propos = []
-var langueActif = "fr"
 
 requestPropos.open('GET', requestProposURL);
 
@@ -16,6 +20,7 @@ requestPropos.onload = function() {
     putTexte()
 }
 
+// Fonction affichant les textes contenues dans la page propos.html dans la langue actif
 function putTexte() {
     const titreHistoire = document.getElementById('NotreHistoire')
     titreHistoire.innerText = propos["titreHistoire"][langueActif]
@@ -36,28 +41,34 @@ function putTexte() {
     legende2.innerText = propos["legende2"][langueActif]
 }
 
+// Fonction permettant de changer la langue du contenu de la page index.html
 function changeLanguePropos(lang) {
-    langueActif = lang
-    putTexte()
-
+    langueActif = lang // Changement de la langue actif
+    putTexte() // Appel de la fonction putTexte() 
 }
 
+// Evènement correspondant au click sur le drapeau français
 document.getElementById('BtnFr').addEventListener(
     'click',
+    // Traduction en français des services grâce à la fonction changeLanguePropos()
     function() {
         changeLanguePropos('fr')
     }
 )
 
+// Evènement correspondant au click sur le drapeau anglais
 document.getElementById('BtnAng').addEventListener(
     'click',
+    // Traduction en anglais des services grâce à la fonction changeLanguePropos()
     function() {
         changeLanguePropos('en')
     }
 )
 
+// Evènement correspondant au click sur le drapeau espagnol
 document.getElementById('BtnEsp').addEventListener(
     'click',
+    // Traduction en espagnol des services grâce à la fonction changeLanguePropos()
     function() {
         changeLanguePropos('es')
     }
