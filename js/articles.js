10d29b31e39939a32fb4b4b4479428c285a81817
GuilhemGabrielli <guilhem.gabrielli@campus.academy>
2022-01-20 15:21:29 +0100

    Update articles.js

--- js/articles.js
+++ js/articles.js
@@ -1,10 +1,14 @@
+// Variables globales
+var articles = []
+var langueActif = "fr"
+
+// Désactivation de lien possédant l'id "page-actuel", cela empêche l'utilisateur d'ouvrir la page sur laquelle il se trouve
 document.getElementById('page-actuel').removeAttribute('href')
 
+// Partie exécuté au lancement de la page permettant de récupérer les données JSON stockées sur GitHub et de les convertir en JS
 var requestArticlesURL = 'https://raw.githubusercontent.com/CampusWorkshop2022-team04/Digital-Consultant/main/json/articles.json';
 
 var requestArticles = new XMLHttpRequest();
-var articles = []
-var langueActif = "fr"
 
 requestArticles.open('GET', requestArticlesURL);
 
@@ -13,15 +17,16 @@ requestArticles.send();
 
 requestArticles.onload = function() {
     articles = requestArticles.response;
-    presentArticle()
+    presentArticle() // Présentation des articles
 }
 
-
+// Fonction affichant les articles du blog.html
 function presentArticle() {
     const main = document.getElementById('main')
     const hArticle = document.createElement('h1')
     hArticle.id = "titleArticles"
 
+    // On vérifie si il y a des articles à afficher
     if (articles.length == 0) {
         hArticle.innerText = "Désolé, aucun article n'est actuellement disponible"
     } else {
@@ -31,6 +36,8 @@ function presentArticle() {
     main.prepend(hArticle)
 }
 
+
+// Fonction ajoutant la totalité des articles dans l'élément html passé en paramètre danz la langue actif
 function addArticles(divArticle) {
     console.log(articles.length)
     for (let i=1; i<articles.length; i++) {
@@ -53,6 +60,7 @@ function addArticles(divArticle) {
 
 }
 
+// Fonction ajoutant une partie commentaire à chaque élément html passé en paramètres
 function addcommentaire(divCommentaire) {
     const ajoutCommentaire = document.createElement('form')
     ajoutCommentaire.className = "AjoutCommentaire"
@@ -75,32 +83,40 @@ function addcommentaire(divCommentaire) {
     divCommentaire.append(ajoutCommentaire)
 }
 
+// Fonction permettant de changer de langue
 function changeLangueArticle(langue) {
     langueActif = langue
-    document.getElementById('titleArticles').innerText = articles[0]["titreArticle"][langueActif]
-    const arr = document.getElementsByClassName('commentaireArticleBlog')
+    document.getElementById('titleArticles').innerText = articles[0]["titreArticle"][langueActif] // Traduction du titre bu blog avec la langue sélectionnée
+    const arr = document.getElementsByClassName('commentaireArticleBlog') // Tableau possédant les éléments html contenant les descriptions des solutions
 
+    //Cette boucle tourne en fonction du nombre d'article et traduit les desciptions de chaque article dans la langue selectionnée
     for (let i=0; i<arr.length;i++) {
         arr[i].innerText = articles[i+1]["commentaire"][langueActif]
     }
 }
 
+// Evènement correspondant au click sur le drapeau français
 document.getElementById('BtnFr').addEventListener(
     'click',
+    // Traduction en français des services grâce à la fonction changeLangueArticle()
     function() {
         changeLangueArticle('fr')
     }
 )
 
+// Evènement correspondant au click sur le drapeau anglais
 document.getElementById('BtnAng').addEventListener(
     'click',
+    // Traduction en anglais des services grâce à la fonction changeLangueArticle()
     function() {
         changeLangueArticle('en')
     }
 )
 
+// Evènement correspondant au click sur le drapeau espagnol
 document.getElementById('BtnEsp').addEventListener(
     'click',
+    // Traduction en espagnol des services grâce à la fonction changeLangueArticle()
     function() {
         changeLangueArticle('es')
     }
