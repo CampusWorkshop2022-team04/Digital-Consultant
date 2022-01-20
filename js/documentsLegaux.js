b43ba2050a1c25115859e7353916a16176022d28
GuilhemGabrielli <guilhem.gabrielli@campus.academy>
2022-01-20 14:55:44 +0100

    Update documentsLegaux.js

--- js/documentsLegaux.js
+++ js/documentsLegaux.js
@@ -1,7 +1,10 @@
+// Variable globale
+var documentsLegaux = []
+
+// Partie exécuté au lancement de la page permettant de récupérer les données JSON stockées sur GitHub et de les convertir en JS
 var requestDocumentsLegauxURL = 'https://raw.githubusercontent.com/CampusWorkshop2022-team04/Digital-Consultant/main/json/documentsLegaux.json';
 
 var requestDocumentsLegaux = new XMLHttpRequest();
-var documentsLegaux = []
 
 requestDocumentsLegaux.open('GET', requestDocumentsLegauxURL);
 
@@ -10,18 +13,18 @@ requestDocumentsLegaux.send();
 
 requestDocumentsLegaux.onload = function() {
     documentsLegaux = requestDocumentsLegaux.response;
-    modificationPrivatePolicy(documentsLegaux)
+    modificationPrivatePolicy()
 }
 
-function modificationPrivatePolicy(documents) {
-
-    const keyJSON = Object.keys(documents)
+// Fonction pour remplir le privacy policy avec les éléments en JSON
+function modificationPrivatePolicy() {
+    const keyJSON = Object.keys(documentsLegaux)
     const keyHTML = documents["classHTML"]
     for (let i=0; i<keyHTML.length; i++) {
-        arr = document.getElementsByClassName(keyHTML[i])
+        arr = documentsLegaux.getElementsByClassName(keyHTML[i])
 
         for (let j=0; j<arr.length; j++) {
-            arr[j].innerText = documents[keyJSON[i+1]]
+            arr[j].innerText = documentsLegaux[keyJSON[i+1]]
         }
     }
 
