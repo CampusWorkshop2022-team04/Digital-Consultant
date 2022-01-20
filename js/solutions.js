da8ac344b4634657598ded6f72bfe8faaba8bbc9
GuilhemGabrielli <guilhem.gabrielli@campus.academy>
2022-01-20 13:55:58 +0100

    Update solutions.js

--- js/solutions.js
+++ js/solutions.js
@@ -1,10 +1,13 @@
-document.getElementById('page-actuel').removeAttribute('href')
-
-var requestSolutionsURL = 'https://raw.githubusercontent.com/CampusWorkshop2022-team04/Digital-Consultant/main/json/solutions.json';
-
+// Variable globale
 var requestSolutions = new XMLHttpRequest();
 var solutions = []
 var langueActif = "fr"
+
+document.getElementById('page-actuel').removeAttribute('href')
+
+// Partie exécuté au lancement de la page permettant de récupérer les données JSON stockées sur GitHub et de les convertir en JS
+var requestSolutionsURL = 'https://raw.githubusercontent.com/CampusWorkshop2022-team04/Digital-Consultant/main/json/solutions.json';
+
 
 requestSolutions.open('GET', requestSolutionsURL);
 
@@ -13,21 +16,20 @@ requestSolutions.send();
 
 requestSolutions.onload = function() {
     solutions = requestSolutions.response;
-    document.getElementById('titreSol').innerText = solutions[0]["titreSolution"][langueActif]
-    presentSolutions(solutions)
+    document.getElementById('titreSol').innerText = solutions[0]["titreSolution"][langueActif] // Affichage du titre des solutions dans la langue actif
+    presentSolutions() // Présentation des solutions
 }
 
-
-function presentSolutions(solu)  {
+// Fonction présentant les différentes solutions propsés par Digital Consultant dans la langue actif
+function presentSolutions()  {
     const nosSolutions = document.getElementById('nosSolutions')
 
-    for (let i= 1; i<solu.length; i++) {
-        addSolution(solu[i], nosSolutions)
+    for (let i= 1; i<solutions.length; i++) {
+        addSolution(solutions[i], nosSolutions)
     }
-
-
 }
 
+// Fonction ajoutant la solution en paramètre dans l'élément html en paramètre dans la langue actif
 function addSolution(solution, divSolutions) {
     const divSolution= document.createElement('div')
     divSolution.className = "divSolution"
@@ -46,35 +48,42 @@ function addSolution(solution, divSolutions) {
     divSolutions.append(divSolution)
 }
 
+// Fonction permettant de changer la langue de la partie solution
 function changeLangueSol(langue) {
-    langueActif = langue
-    document.getElementById('titreSol').innerText = solutions[0]["titreSolution"][langueActif]
-    const arrNomSolution = document.getElementById('nosSolutions').getElementsByClassName('titreSolSer')
-    const arrDescriptionSolution = document.getElementById('nosSolutions').getElementsByClassName('descriptifSolSer')
- 
+    langueActif = langue // Changement de la langue actif
+    document.getElementById('titreSol').innerText = solutions[0]["titreSolution"][langueActif] // Traduction du titre des services avec la langue sélectionnée
+    const arrNomSolution = document.getElementById('nosSolutions').getElementsByClassName('titreSolSer') // Tableau possédant les éléments html contenant les titre des solutions
+    const arrDescriptionSolution = document.getElementById('nosSolutions').getElementsByClassName('descriptifSolSer') // Tableau possédant les éléments html contenant le descriptifs des solutions
+    
+    //Cette boucle tourne en fonction du nombre de solutions et traduit les titres et les desciptions de chaques solutions dans la langue selectionnée
     for (let i=0; i<arrDescriptionSolution.length;i++) {
         arrNomSolution[i].innerText = solutions[i+1]["nomSolution"][langue]
         arrDescriptionSolution[i].innerText = solutions[i+1]["descriptionSolution"][langue]
     }
 }
 
-
+// Evènement correspondant au click sur le drapeau français
 document.getElementById('BtnFr').addEventListener(
     'click',
+    // Traduction en français des services grâce à la fonction changeLangueServ()
     function() {
         changeLangueSol('fr')
     }
 )
 
+// Evènement correspondant au click sur le drapeau anglais
 document.getElementById('BtnAng').addEventListener(
     'click',
+    // Traduction en anglais des services grâce à la fonction changeLangueServ()
     function() {
         changeLangueSol('en')
     }
 )
 
+// Evènement correspondant au click sur le drapeau espagnol
 document.getElementById('BtnEsp').addEventListener(
     'click',
+    // Traduction en espagnol des services grâce à la fonction changeLangueServ()
     function() {
         changeLangueSol('es')
     }
