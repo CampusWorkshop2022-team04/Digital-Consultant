263f4bd6259a5359a163f30b75bf96ec32194c5b
NathanMorard <97445758+NathanMorard@users.noreply.github.com>
2022-01-20 12:40:27 +0100

    Update scriptPOP.js

--- js/scriptPOP.js
+++ js/scriptPOP.js
@@ -1,4 +1,5 @@
-var btnPopup = document.getElementById('btngerer');
+// script permettant au popup d'etre dynamique
+var btnPopup = document.getElementById('btngerer'); 
 var overlay = document.getElementById('overlay');
 var btnclose = document.getElementById('btnclose');
 var btnautoriser = document.getElementById('btnautoriser');
@@ -7,16 +8,20 @@ var textecookies = document.getElementById('textecookies');
 btngerer.addEventListener('click', openModal);
 btnclose.addEventListener('click', closepopup);
 btnautoriser.addEventListener('click', closetexte);
+ 
 
+/** Fonction permettant d'ouvrir la popup grace a EventListener  */
 function openModal(){
     overlay.style.display = 'block'; 
     textecookies.style.display ='none';
 }
 
+/** Fonction permettant de fermer la popup grace a EventListener et l'icone "croix" */
 function closepopup(){
     overlay.style.display = 'none'; 
 }
 
+ /** Fonction permettant de faire disparaitre le texte lorsqu'on ouvre la popup  */
 function closetexte(){
     textecookies.style.display ='none';
 }
